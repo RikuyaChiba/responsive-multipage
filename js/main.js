c87da48884a535e1cf9ba6ecb810a8c69248e1ac
Rikuya Chiba <rikuya.chiba@gmail.com>
2022-07-31 18:48:42 +0900

    Create faqs section

--- js/main.js
+++ js/main.js
@@ -2,5 +2,23 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 80)
+});
 
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+console.log(faqs);
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq__icon span');
+        if(icon.innerText === 'add') {
+            icon.innerText = 'remove';
+        } else {
+            icon.innerText = 'add';
+        }
+    });
 });
